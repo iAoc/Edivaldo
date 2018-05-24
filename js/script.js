30c02741a75579694582d7a8a9b314e8dc40426e
iAoc <38301661+iAoc@users.noreply.github.com>
2018-05-24 19:56:49 -0300

    Update script.js

--- js/script.js
+++ js/script.js
@@ -1,7 +1,1 @@
-const celular = [
-  {search: "iphone", url: "apple.html"},
-  {title: "samsung", url: "samsung.html"},
-  {title: "nokia", url: "nokia.html"},
-  {title: "microsoft", url: "nokia.html"},
-  {title: "motorola", url: "motorola.html"}
-];
+
